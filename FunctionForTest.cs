5c9104c2babd800dde0b7207fada512df5bae496
Flavio <flaviocampelo@hotmail.com>
2022-04-23 06:48:27 -0400

    Adding secrets for this sample

--- FunctionForTest.cs
+++ FunctionForTest.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Graph;
 using Microsoft.Identity.Client;
@@ -13,50 +14,70 @@ using System.Threading.Tasks;
 
 namespace Flavio.FunctionTest
 {
-    public static class FunctionForTest
+  public class FunctionForTest
+  {
+    private readonly IConfiguration _configuration;
+
+    public FunctionForTest(IConfiguration configuration)
     {
-        [FunctionName("FunctionForTest")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
-        {
-            log.LogInformation("C# HTTP trigger FunctionForTest processed a request.");
-
-            string maxString = null;
-            if(req.Method == "GET")
-                maxString = req.Query["max"];
-            else if(req.Method == "POST"){
-                string bodyString = await new StreamReader(req.Body).ReadToEndAsync();
-                dynamic body = Newtonsoft.Json.JsonConvert.DeserializeObject(bodyString);
-                maxString = body?.max;
-            }
-            if (!int.TryParse(maxString, out int max))
-                return new BadRequestObjectResult(new {Error = "Invalid or not found MAX param"});
-
-            string[] scopes = new string[] { "https://graph.microsoft.com/.default" };
-
-            IConfidentialClientApplication msalClient = ConfidentialClientApplicationBuilder
-                .Create(Environment.GetEnvironmentVariable("ClientId"))
-                .WithAuthority(AadAuthorityAudience.AzureAdMyOrg, true)
-                .WithTenantId(Environment.GetEnvironmentVariable("TenantId"))
-                .WithClientSecret(Environment.GetEnvironmentVariable("ClientSecret"))
-                .Build();
-
-            string token = null;
-            AuthenticationResult cacheResult = await msalClient.AcquireTokenForClient(scopes).ExecuteAsync();
-            token = cacheResult.AccessToken;
-
-            GraphServiceClient graphClient = new GraphServiceClient(new DelegateAuthenticationProvider(
-                async (requestMessage) =>
-                {
-                    requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
-                    await Task.FromResult(0);
-                }));
-
-            var response = await graphClient.Users.Request().GetAsync();
-
-            return new OkObjectResult(new { Result = response.CurrentPage.Take(max)
-                .Select(u => new { u.Id, u.DisplayName, u.GivenName, LastName = u.Surname }) });
-        }
+      _configuration = configuration;
     }
+
+    [FunctionName("FunctionForTest")]
+    public async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+        ILogger log)
+    {
+      log.LogInformation("C# HTTP trigger FunctionForTest processed a request.");
+
+      string clientId = _configuration["ClientId"];
+      string clientSecret = _configuration["ClientSecret"];
+      string tenantId = _configuration["TenantId"];
+
+      if (string.IsNullOrEmpty(clientId) || string.IsNullOrEmpty(clientSecret) || string.IsNullOrEmpty(tenantId))
+      {
+        return new BadRequestObjectResult(new { Error = "Please set ClientId, ClientSecret and TenantId. https://docs.microsoft.com/en-us/aspnet/core/security/app-secrets" });
+      }
+
+      string maxString = null;
+      if (req.Method == "GET")
+        maxString = req.Query["max"];
+      else if (req.Method == "POST")
+      {
+        string bodyString = await new StreamReader(req.Body).ReadToEndAsync();
+        dynamic body = Newtonsoft.Json.JsonConvert.DeserializeObject(bodyString);
+        maxString = body?.max;
+      }
+      if (!int.TryParse(maxString, out int max))
+        return new BadRequestObjectResult(new { Error = "Invalid or not found MAX param" });
+
+      string[] scopes = new string[] { "https://graph.microsoft.com/.default" };
+
+      IConfidentialClientApplication msalClient = ConfidentialClientApplicationBuilder
+          .Create(clientId)
+          .WithAuthority(AadAuthorityAudience.AzureAdMyOrg, true)
+          .WithTenantId(tenantId)
+          .WithClientSecret(clientSecret)
+          .Build();
+
+      string token = null;
+      AuthenticationResult cacheResult = await msalClient.AcquireTokenForClient(scopes).ExecuteAsync();
+      token = cacheResult.AccessToken;
+
+      GraphServiceClient graphClient = new GraphServiceClient(new DelegateAuthenticationProvider(
+          async (requestMessage) =>
+          {
+            requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            await Task.FromResult(0);
+          }));
+
+      var response = await graphClient.Users.Request().GetAsync();
+
+      return new OkObjectResult(new
+      {
+        Result = response.CurrentPage.Take(max)
+          .Select(u => new { u.Id, u.DisplayName, u.GivenName, LastName = u.Surname })
+      });
+    }
+  }
 }
--- README.md
+++ README.md
@@ -2,7 +2,24 @@
 
 This sample uses a MicrosoftGraph client credentials flow to get users on AD
 
-To use it, you can make a GET or POST to the app URL like those samples
+## Configuring your client credentials
+
+First of all, you should add some [secret configurations](https://docs.microsoft.com/en-us/aspnet/core/security/app-secrets) before running this function.
+
+Init your secrects 
+
+```powershell
+dotnet user-secrets init
+```
+
+And add your three new values
+```powershell
+dotnet user-secrets set "tenantId" "{MY_TENANT_ID}" # It's a GUID value like '12345678-1234-1234-1234-1234567890ab'. You can get this value on your tenant's overview page. https://aad.portal.azure.com/
+dotnet user-secrets set "clientId" "{MY_CLIENT_ID}" # It's a GUID value like '12345678-1234-1234-1234-1234567890ab'. You can get this value on your app registration's overview page. https://portal.azure.com/
+dotnet user-secrets set "clientSecret" "{MY_CLIENT_SECRET}" # It's like an ecrypted password Q~nfpjRObkLeRjQsyD. You can get this value once when you create a new client secret on your app registration's page. https://portal.azure.com/
+```
+
+After that you be able to make calls to this function. For this sample, you can make a GET or POST requests.
 
 ## GET
 
